5cdc352373da70555bab65439cf0f1832f1e40e8
m@lbit <michal@arqma.com>
2025-03-11 13:04:24 +0100

    update

--- cmake/local-libzmq/FindZeroMQ.cmake
+++ cmake/local-libzmq/FindZeroMQ.cmake
@@ -5,4 +5,3 @@ add_library(libzmq-static INTERFACE IMPORTED GLOBAL)
 target_link_libraries(libzmq INTERFACE libzmq_vendor)
 target_link_libraries(libzmq-static INTERFACE libzmq_vendor)
 set(ZeroMQ_FOUND ON)
-
